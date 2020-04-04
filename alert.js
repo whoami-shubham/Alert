53275d986e71ce9bb0c078db6aab932ab83fa52c
whoami-shubham <shubhamkumarjha0013@gmail.com>
2020-04-04 14:13:20 +0530

    :tada: chaining of alerts added

--- alert.js
+++ alert.js
@@ -1,9 +1,8 @@
-
-function createAlert(title,message){
-    let heading = title!==undefined?`<h3>${title}</h3>`:'';
-    let msg_class = title===undefined?'heading':'';
-    let element = `
-    <div class='alert-box col bounce'>
+function createAlert(title, message) {
+  let heading = title !== undefined ? `<h1>${title}</h1>` : "";
+  let msg_class = title === undefined ? "heading" : "";
+  let element = `
+    <div class='alert-box col bounce mg-0'>
         ${heading}
        <p class='row flex-center ${msg_class}'>${message}</p>
        <div class='row flex-center'>
@@ -11,44 +10,55 @@ function createAlert(title,message){
        </div>
     </div>
 
-    `
-    return element;
-    
+    `;
+  return element;
 }
 
-function Alert(args){
-    let {title,message,response} = args!==undefined?args:{};
-    if(response===undefined){
-        response = {};
+async function Alert(args) {
+  let { title, message, callBacks } = args !== undefined ? args : {};
+  let response = {result:'none'};
+  if (message === undefined && title == undefined) {
+    message = "Hello World !";
+  }
+  response.result = false;
+  let container = document.createElement("div");
+  container.classList.add("alert");
+  container.innerHTML = createAlert(title, message);
+  document.body.appendChild(container);
+  let ok = document.querySelector(".alert-ok");
+  let cancel = document.querySelector(".alert-cancel");
+  ok.addEventListener("click", Ok(container, response, callBacks));
+  cancel.addEventListener("click", Cancel(container, response));
+  return response;
+}
+
+function Ok(container, response, callBacks) {
+  return async () => {
+    //console.log('ok')
+    var new_element = document.createElement("div");
+    container.parentNode.replaceChild(new_element, container);
+    response.result = 'ok';
+    if (callBacks) {
+      let start = 0,end = callBacks.length; 
+      let res = await callBacks[start++]();
+      let iterate = setInterval(async ()=>{
+        if(start===end || res.result==='cancel'){
+            clearInterval(iterate);
+            //console.log('clearinterval')
+        }
+        if(res.result==='ok'){
+            res = await callBacks[start++]();
+        }
+      },10);
+
     }
-    if(message===undefined && title==undefined){
-        message = "Hello World !";
-    }
-    response.result = false;
-    let container = document.createElement('div');
-    container.classList.add('alert');
-    container.innerHTML = createAlert(title,message);
-    document.body.appendChild(container);
-    let ok = document.querySelector('.alert-ok');
-    let cancel = document.querySelector('.alert-cancel');
-    ok.addEventListener('click',Ok(container,response));
-    cancel.addEventListener('click',Cancel(container,response));
-
+  };
 }
-
-function Ok(container,response){
-       return ()=>{
-           //console.log('ok')
-           var new_element = document.createElement('div');
-           container.parentNode.replaceChild(new_element, container);
-           response.result = true;
-       }
-}
-function Cancel(container,response){
-       return ()=>{
-           //console.log('Cancel')
-           var new_element = document.createElement('div');
-           container.parentNode.replaceChild(new_element, container);
-           response.result = false;
-       }
+function Cancel(container, response) {
+  return () => {
+    //console.log('Cancel')
+    var new_element = document.createElement("div");
+    container.parentNode.replaceChild(new_element, container);
+    response.result = 'cancel';
+  };
 }
